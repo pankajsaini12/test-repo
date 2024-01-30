f7260971ad2972f6d9c1d4a750bd92c4497a3975
Pankaj Saini <52159526+pankajsaini12@users.noreply.github.com>
2024-01-30 12:12:09 +0530

    Update settings.py

--- django_gpt/settings.py
+++ django_gpt/settings.py
@@ -74,10 +74,6 @@ WSGI_APPLICATION = 'django_gpt.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': BASE_DIR / 'db.sqlite3',
-    # }
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'rasadb',
