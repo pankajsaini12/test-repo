49e0781f8d9a80b5271e69e3355b043a1d6285fb
Pankaj Saini <52159526+pankajsaini12@users.noreply.github.com>
2024-01-30 14:38:40 +0530

    Update settings.py

--- django_gpt/settings.py
+++ django_gpt/settings.py
@@ -11,6 +11,11 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import environ
+
+# Initialise environment variables
+env = environ.Env()
+environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-5_v)jx0#o9-4tja1mux(iyj^32o2x0pei7-7y*z2q&+^)r(1#a'
+SECRET_KEY = env.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
