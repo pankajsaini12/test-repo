40c0883ffda64c3f40445ff8d44281c0edf5d88d
Pankaj Saini <52159526+pankajsaini12@users.noreply.github.com>
2024-01-30 15:08:17 +0530

    Update settings.py

--- django_gpt/settings.py
+++ django_gpt/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env.get('SECRET_KEY')
+SECRET_KEY = env['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
@@ -84,7 +84,7 @@ DATABASES = {
         'NAME': 'rasadb',
         'USER': 'root',
         'PASSWORD': 'password',
-        'HOST': 'ansible_demo_db_1',
+        'HOST': 'db_1',
         'PORT': '3306',
     }
 }
